a2e98f93c495ae1532f9c0d2c20ed9ac68338cf7
Richard Whitmer <richard@panchesco.com>
2020-02-12 12:46:58 -0700

    Updates addon for EE5 Compatibility

---
+++ EE5/system/user/addons/nce_ldap/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+  'author'      => 'Richard Whitmer/Godat Design, Inc.',
+  'author_url'  => 'https://github.com/Panchesco',
+  'name'        => 'NCE LDAP',
+  'description' => 'An ExpressionEngine Extension that allows the authentication of users via LDAP. LDAP details are copied to the EE database before standard MySQL authentication is performed. If user is not found on LDAP, MySQL authentication will still be performed (useful for EE users not in LDAP).',
+  'version'     => '1.0.0',
+  'namespace'   => 'Nce\Nce_Ldap',
+  'settings_exist' => TRUE
+);
